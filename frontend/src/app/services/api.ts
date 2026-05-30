57401e187eaaf31e6d2318300aadaa5036260dd6
tinilinsk <tema.ryabkov@gmail.com>
2026-05-30 23:32:29 +0200

    Added services for auth in angular

---
+++ frontend/src/app/services/api.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Api {
+  private url = "http://localhost:8080";
+
+  constructor(private http: HttpClient) {}
+
+  registerUser(data: any) {
+    this.http.post(`${this.url}/api/auth/register`, data);
+  }
+
+  loginUser(data: any) {
+    this.http.post(`${this.url}/api/auth/login`, data)
+  }
+  
+}
