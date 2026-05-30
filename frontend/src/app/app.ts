7daa5437df5cb7351b878cec7c11d81783b9b851
tinilinsk <tema.ryabkov@gmail.com>
2026-05-31 00:10:32 +0200

    Added services api and req for register in app

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Api } from './services/api';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,20 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.css'
 })
 export class App {
+      constructor(private api: Api) {
+    }
+
     submit(form: any) {
       const value = form.value;
       console.log(value)
+
+      this.api.registerUser(value).subscribe({
+        next: (res) => {
+          console.log("Added new user")
+        },
+        error: (err) => {
+          console.log("Error: " + err)
+        }
+      })
     }
 }
