d774dcf31f3781a3aa5024380c0bfa42004d7f0c
tinilinsk <tema.ryabkov@gmail.com>
2026-05-31 23:55:20 +0200

    Added checked email for registration

--- frontend/src/app/registration/registration.ts
+++ frontend/src/app/registration/registration.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Auth } from '../services/auth';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
-import { inject } from '@angular/core';
+import { inject, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-registration',
@@ -11,6 +11,11 @@ import { inject } from '@angular/core';
   styleUrl: './registration.css',
 })
 export class Registration {
+
+  invalid: boolean = false;
+
+  private cdr = inject(ChangeDetectorRef);
+
     private router = inject(Router);
 
     constructor(private ayth: Auth) {
@@ -20,10 +25,13 @@ export class Registration {
       const value = form.value;
       this.ayth.registerUser(value).subscribe({
         next: (res) => {
+          this.invalid = false;
+          this.cdr.detectChanges();
           this.router.navigateByUrl('/')
         },
         error: (err) => {
-          console.log("Error: " + err)
+          this.invalid = true;
+          this.cdr.detectChanges();
         }
       })
     }
