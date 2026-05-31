6ecdab06484369ec090cc50bb085d17d60e3e999
tinilinsk <tema.ryabkov@gmail.com>
2026-05-31 20:11:50 +0200

    Added login page with logics

---
+++ frontend/src/app/login/login.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { Auth } from '../services/auth';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+  private router = inject(Router);
+
+  constructor(private auth: Auth) {
+
+  }
+
+  submit(form: any) {
+    const value = form.value;
+    console.log(value)
+
+    this.auth.loginUser(value).subscribe({
+        next: (res) => {
+          console.log("Succeses login")
+          this.router.navigate(['home'])
+        },
+        error: (err) => {
+          console.log("Error: " + err)
+        }
+      })
+    }
+}
