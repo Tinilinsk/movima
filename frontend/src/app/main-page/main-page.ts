933508539cdf68d5e02c4d2e0bca54d758c62728
tinilinsk <tema.ryabkov@gmail.com>
2026-06-05 00:17:56 +0200

    Added show movies from db

--- frontend/src/app/main-page/main-page.ts
+++ frontend/src/app/main-page/main-page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Auth } from '../services/auth';
+import { Movie } from '../services/movie';
 
 @Component({
   selector: 'app-main-page',
@@ -7,10 +8,25 @@ import { Auth } from '../services/auth';
   templateUrl: './main-page.html',
   styleUrl: './main-page.css',
 })
-export class MainPage {
-  constructor(private auth: Auth) {
 
-  }
+export class MainPage {
+
+  movies = signal<any[]>([]);
+
+  constructor(private auth: Auth, private movie: Movie) {
+      this.movie.getAllMovies().subscribe({
+      next: (data) => {
+        this.movies.set(data);
+        console.log("Here is your actual backend data:", this.movies()); 
+      },
+      error: (err) => {
+        console.error("HTTP Request failed!", err);
+      }
+    });
+    }
+  
+
+  
 
   logout() {
     this.auth.logoutUser();
