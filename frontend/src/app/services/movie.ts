236f32cdd13f00b99fb7b9bbc6747e122ee7ac9e
tinilinsk <tema.ryabkov@gmail.com>
2026-06-04 23:43:18 +0200

    Added services for movie

---
+++ frontend/src/app/services/movie.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Movie {
+  private url = "http://localhost:8080";
+
+  constructor(private http: HttpClient) {}
+  
+  getAllMovies() {
+    return this.http.get(`${this.url}/api/movies`)
+  }
+}
