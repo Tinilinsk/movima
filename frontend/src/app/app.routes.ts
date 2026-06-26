6752462ae3d576ab26ea7acbb8ef95afb46d7511
tinilinsk <tema.ryabkov@gmail.com>
2026-06-26 16:25:11 +0200

    Added item path for routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { MainPage } from './pages/main-page/main-page';
 import { Login } from './pages/login/login';
 import { WatchedPage } from './pages/watched-page/watched-page';
 import { authGuard } from './guard/auth-guard';
+import { Item } from './pages/item/item';
 
 export const routes: Routes = [
     {
@@ -22,5 +23,9 @@ export const routes: Routes = [
     {
         path:'watched',
         component: WatchedPage
+    },
+    {
+        path: 'movies/:id',
+        component: Item
     }
 ];
