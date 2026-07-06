8f2edc6771b0d48c4a98bca63096ae7941081113
tinilinsk <tema.ryabkov@gmail.com>
2026-07-06 15:23:01 +0200

    Added enviroment for api key

---
+++ frontend/src/environments/environment.example.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080',
+  tmdbApiKey: 'YOUR_TMDB_API_KEY_HERE',
+  tmdbBaseUrl: 'https://api.themoviedb.org/3',
+  tmdbImageUrl: 'https://image.tmdb.org/t/p/w500'
+};
